81845cd0ca36466f630382f09bee52a4fb87d68d
FelpBravo <83423125+FelpBravo@users.noreply.github.com>
2021-07-09 14:52:49 -0400

    Titulo
    Modificación de toda la data.. 80%

--- selenium-test/Generar Allure Results y Screenshots.bat
+++
@@ -1,9 +0,0 @@
-echo. ##########TESTS LOGIN ALLURE#############
-O:
-cd O:\physharm\selenium-test
-.\environment\Scripts\activate.bat
-cd .\src\tests
-allure.bat generate ..\allure-results --output ..\allure-report --clean
-cd..
-allure open --port 5000
-pause
